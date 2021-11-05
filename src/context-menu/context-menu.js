fb15324a5213c10475f4d37c9a8c418653116bee
Frederik S. Held <git@frederikheld.de>
2021-11-05 15:09:02 +0100

    Adss enabled/disabled state for tab context menu

--- src/context-menu/context-menu.js
+++ src/context-menu/context-menu.js
@@ -1,19 +1,28 @@
 'use strict'
 
-import { addToQuickAccessStore } from '../functions/quick-access-store.js'
+import { addToQuickAccessStore, isInQuickAccessStore } from '../functions/quick-access-store.js'
 
 browser.contextMenus.create({
-    id: 'highlight-tab',
+    id: 'add-tab-to-quick-access-store',
     title: browser.i18n.getMessage('contextMenuName'),
     contexts: ['tab']
 })
 
 browser.contextMenus.onClicked.addListener(async function (info, tab) {
     switch (info.menuItemId) {
-        case 'highlight-tab':
-            console.log(info)
-            console.log(tab)
+        case 'add-tab-to-quick-access-store':
             await addToQuickAccessStore(tab.id)
             break
     }
 })
+
+browser.contextMenus.onShown.addListener(async function (info, tab) {
+    const inQuickAccessStore = await isInQuickAccessStore(tab.id)
+
+    browser.contextMenus.update('add-tab-to-quick-access-store', {
+        enabled: !inQuickAccessStore
+    })
+    
+    browser.contextMenus.refresh()
+})
+// DOCS: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/menus/onShown
